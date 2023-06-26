97f106c003a4bff94577258140cd149b9186a101
treeaa <89380000+treeaa@users.noreply.github.com>
2023-06-26 17:09:31 +0800

    Update display.ino

--- examples/display/display.ino
+++ examples/display/display.ino
@@ -16,6 +16,7 @@ uint8_t t = ' ';
 
 BMD31M090     BMD31(BMD31M090_WIDTH, BMD31M090_HEIGHT, &Wire); //Please uncomment out this line of code if you use HW Wire on BMduino
 //BMD31M090     BMD31(BMD31M090_WIDTH, BMD31M090_HEIGHT, &Wire1); //Please uncomment out this line of code if you use HW Wire1 on BMduino
+//BMD31M090     BMD31(BMD31M090_WIDTH, BMD31M090_HEIGHT, &Wire2); //Please uncomment out this line of code if you use HW Wire1 on BMduino
 
 void setup()
 {
